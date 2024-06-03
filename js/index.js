062971d5462bcf7a7818e6e5a58ec0dd1869f858
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-06-03 23:05:46 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,127 @@
+/**
+ * 1-masala
+ * let num = +prompt("3 xonali son");
+ * let y= Math.trunc(num / 100 );
+ * console.log(y)
+ */
+
+/**
+ * 2-masala
+ * let num = +prompt("3 xonali son");
+ * let b = num % 10;
+ * let o = Math.trunc(num / 10) % 10;
+ * console.log(b);
+ * console.log(o);
+ */
+
+/**
+ * 3-masala
+ * let num = +prompt("3 xonali son");
+ * let b = num % 10;
+ * let o = Math.trunc(num / 10) % 10;
+ * let y= Math.trunc(num / 100 );
+ * let sum= b + o + y;
+ * console.log(sum);
+ */
+
+/**
+ * 4-masala
+ * let num = +prompt("3 xonali son");
+ * let b = num % 10;
+ * let o = Math.trunc(num / 10) % 10;
+ * let y= Math.trunc(num / 100 );
+ * let res= b * 100 + o * 10 + y;
+ * console.log(res);
+ */
+
+/**
+ * 5-masala
+ * let num = +prompt("3 xonali son");
+ * let b = num % 10;
+ * let o = Math.trunc(num / 10) % 10;
+ * let y= Math.trunc(num / 100 );
+ * let res= b * 100 + y * 10 + o;
+ * console.log(res);
+ */
+
+/**
+ * 6-masala
+ * let num = +prompt("3 xonali son");
+ * let b = num % 10;
+ * let o = Math.trunc(num / 10) % 10;
+ * let y= Math.trunc(num / 100 );
+ * let res= o * 100 + y * 10 + b;
+ * console.log(res);
+ */
+
+/**
+ * 7-masala
+ * let num = +prompt("3 xonali son");
+ * let b = num % 10;
+ * let o = Math.trunc(num / 10) % 10;
+ * let y= Math.trunc(num / 100 );
+ * let res= o * 100 + y * 10 + b;
+ * console.log(res);
+ */
+
+/**
+ * 8-masala
+ * let num = +prompt("3 xonali son");
+ * let b = num % 10;
+ * let o = Math.trunc(num / 10) % 10;
+ * let y= Math.trunc(num / 100 );
+ * let res= y * 100 + b * 10 + o;
+ * console.log(res);
+ */
+
+/**
+ * 9-masala
+ * let num = +prompt("999 > son berilgan")
+ * let res = Math.trunc(num / 100) % 10;
+ * console.log(res);
+ */
+
+/**
+ * 10-masala
+ * let num = +prompt("999 > son berilgan")
+ * let res = Math.trunc(num / 100);
+ * console.log(res);
+ */
+
+
+// 11-masala
+// let N = +prompt("qanchadir sekund");
+// let m = Math.trunc(N / 60);
+// console.log(m);
+
+
+/**
+ * 12-masala
+ * let N = +prompt("qanchadir sekund");
+ * let s = Math.trunc(N / 3600);
+ * console.log(s);
+ */
+
+/**
+ * 13-masala
+ * let N = +prompt("qanchadir sekund");
+ * let m = Math.trunc(N / 60);
+ * let sek = N % 60;
+ * console.log(m +"min" + sek +"sekund");
+ */
+
+/** 
+ * 14-masala
+ * let N = +prompt("qanchadir sekund");
+ * let s = Math.trunc(N / 3600);
+ * let sek = N % 60;
+ * console.log(s +"saot" + sek +"sekund");
+*/
+
+
+//   15-masala
+// let N = +prompt("qanchadir sekund");
+// let s = Math.trunc(N / 3600);
+// let m = Math.trunc(N / 60) % 60;
+// let sek = N % 60;
+// console.log(s + "saot" + m +"min" + sek +"sekund");
